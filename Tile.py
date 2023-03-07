373598b3ac4dd0918b0e767cfdd9729ce88afa26
Androo <84476053+Anduwu1@users.noreply.github.com>
2023-03-07 13:37:16 -0700

    Update Tile.py

--- Tile.py
+++ Tile.py
@@ -4,7 +4,7 @@ class Tile:
   pos_states = [ice_high, ice_medium, ice_low, solar, hazard]
   states = []
   def __init__():
-    geoconstraints = randint(0, 5)
+    geoconstraints = randint(0, len(pos_states))
     for i in range(geoconstraints):
       loc = randint(0, len(pos_states))
       states.append(pos_states[loc])
