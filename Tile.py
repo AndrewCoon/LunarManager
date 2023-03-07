bc6a2b2794ff8218111c85ae947ee86def28835c
Androo <84476053+Anduwu1@users.noreply.github.com>
2023-03-07 13:36:32 -0700

    Create Tile.py

---
+++ Tile.py
@@ -0,0 +1,13 @@
+import random
+
+class Tile:
+  pos_states = [ice_high, ice_medium, ice_low, solar, hazard]
+  states = []
+  def __init__():
+    geoconstraints = randint(0, 5)
+    for i in range(geoconstraints):
+      loc = randint(0, len(pos_states))
+      states.append(pos_states[loc])
+      pos_states.pop(loc)
+      
+            
